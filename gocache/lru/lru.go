a6e7cdaa33c82c7d7cc989b7cdb06b9aa7998f3c
kj5xm <kj5xm@users.noreply.github.com>
2021-08-03 17:07:09 -0400

    init

---
+++ gocache/lru/lru.go
@@ -0,0 +1,33 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes  int64
+	nbytes    int64
+	ll        *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, OnEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: OnEvicted,
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+
+}
